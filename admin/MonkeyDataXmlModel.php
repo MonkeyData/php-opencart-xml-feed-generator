b6c9aeeb61835e24dd01d03b8666afdfd8fa0135
TomasW <tomas.widlak@gmail.com>
2016-03-04 12:39:17 +0100

    update opencart version

--- admin/MonkeyDataXmlModel.php
+++ admin/MonkeyDataXmlModel.php
@@ -54,7 +54,7 @@ class MonkeyDataXmlModel extends XmlModel implements CurrentXmlModelInterface {
     protected $customers = array();
 
     public function authenticateHash($hash) {
-        if (version_compare(VERSION, '2.0', '>=')) {
+        if (version_compare(VERSION, '2.0.1.0', '>=')) {
             $hash_real = $this->connection->query("SELECT `value` FROM {$this->getTableName('setting')} WHERE `code` = 'monkey_data_tmp' && `key` = 'monkey_data_hash' LIMIT 0,1;")->fetchObject();
         } elseif (version_compare(VERSION, '1.5', '>=')) {
             $hash_real = $this->connection->query("SELECT `value` FROM {$this->getTableName('setting')} WHERE `group` = 'monkey_data' && `key` = 'hash' LIMIT 0,1;")->fetchObject();
